b168184c076d982b8e2af9f2048c5190ef961999
Remko Weijnen <r.weijnen@gmail.com>
2024-08-06 10:45:27 +0200

    Update EnableImageCreator.ps1
    Added some debug output, set symbol loading to none to speed up WinDbg

--- EnableImageCreator.ps1
+++ EnableImageCreator.ps1
@@ -9,6 +9,7 @@
 # See https://learn.microsoft.com/en-us/windows/win32/intl/table-of-geographical-locations
 # Assumes 64-bit so uses the rcx register
 $winDbgScript = @'
+.symopt+ 0x40
 bp kernelbase!GetGeoInfoW ".if (@$t0 == 0) {r @$t0 = 0}; .echo 'Hit count:'; r @$t0 = @$t0 + 1; .if (@rcx != 0xF4) { .echo 'Modifying rcx to 0xF4'; r @rcx = 0xF4 }; .if (@$t0 == 4) { .echo 'Detaching after 4 hits'; qqd } .else { g }"
 g
 '@
@@ -27,8 +28,9 @@ Set-Content -Path $scriptFile -Value $winDbgScript -ErrorAction Stop
 
 try 
 {
-    # Let's figure out where MSPaint is installed ...
-    $mspaintPackage = Get-AppxPackage -Name *Microsoft.Paint* | Select-Object -First 1
+	 # Let's figure out where MSPaint is installed ...
+ 	 "Searching for Microsoft Paint..."
+	 $mspaintPackage = Get-AppxPackage -Name *Microsoft.Paint* | Select-Object -First 1
 
     # Check if the Paint app package was found
     if (-not $mspaintPackage) 
@@ -36,9 +38,12 @@ try
         throw "Paint app not found. Exiting script."
     }
 
-    $mspaintPath = Join-Path $mspaintPackage.InstallLocation "PaintApp\mspaint.exe"
+    "Found Microsoft Paint at: $($mspaintPackage.InstallLocation)"
+	 $mspaintPath = Join-Path $mspaintPackage.InstallLocation "PaintApp\mspaint.exe"
 
-    # Define the path for WinDbg Preview using LocalAppData special folder
+    
+	 "Searching for WinDbg(X)..."
+	 # Define the path for WinDbg Preview using LocalAppData special folder
     $windbgPreviewPath = Join-Path $env:LOCALAPPDATA "Microsoft\WindowsApps\WinDbgX.exe"
 
     # Determine the WinDbg path to use
@@ -58,8 +63,31 @@ try
         throw "WinDbg(X) not found. Please ensure it is installed."
     }
 
+	 "Found WingDbg(X) at: $($windbgPath)"
     # Launch WinDbg with the determined path and arguments
-    Start-Process -FilePath $windbgPath -ArgumentList $arguments -Wait
+    
+	 "Start WinDbg(X) and wait until if finishes"
+	 
+	 $psi = New-Object System.Diagnostics.ProcessStartInfo
+	 $psi.FileName = $windbgPath
+	 $psi.RedirectStandardError = $true
+	 $psi.RedirectStandardOutput = $true
+	 $psi.UseShellExecute = $false
+	 $psi.Arguments = $arguments
+	 $process = New-Object System.Diagnostics.Process
+	 $process.StartInfo = $psi
+	 $process.Start() | Out-Null
+	 $process.WaitForExit()
+	 
+	 if (0 -ne $process.ExitCode)
+	 {
+	 	
+		"WinDbg(X) failed with error: $(New-Object System.ComponentModel.Win32Exception($process.ExitCode))"
+	 }
+	 else
+	 {
+	 	"No errors returned"
+	 }
 }
 finally 
 {
